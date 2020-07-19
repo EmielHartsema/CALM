11d1ba7242cb2d38336c8af6620e9c21fb0776a8
EmielHartsema <33688872+EmielHartsema@users.noreply.github.com>
2020-07-19 13:53:51 +0200

    Add files via upload

---
+++ CALM.m
@@ -0,0 +1,26 @@
+%{
+-----------------------------------------------------
+CALM - Computational Applicatoin for Liquid Mechanics
+Author: Emiel Hartsema
+Version: 0.0.1
+Date: 16-4-2020
+-----------------------------------------------------
+%}
+
+% Read settings
+settings = jsondecode(fileread(settings.json));
+
+% Load mesh
+
+
+% assemble matrix
+
+
+% Solve system
+
+
+% Post Processing
+
+
+% Display solution
+
---
+++ Solvers/ico_ns.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ico_ns(inputArg1,inputArg2)
+%ICO_NS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
