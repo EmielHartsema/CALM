e8d22aa01e66f3363536d254bfaf0f9f6cd20830
Emiel <Emiel@Desktop-Emiel>
2020-09-05 19:59:25 +0200

    simulation settings now read from json file

--- CALM.m
+++ CALM.m
@@ -7,8 +7,6 @@ Date: 16-4-2020
 -----------------------------------------------------
 %}
 
-%Simulation parameters
-n_it = 10;
 % add path to libraries
 addpath('readmsh');
 addpath('FEM_lib')
@@ -16,6 +14,7 @@ addpath('FEM_lib')
 % Read settings
 proj_folder = uigetdir;
 %proj_folder = 'C:\Users\Emiel\Documents\CALM\Testcase1_Cilinder_flow';
+myCFD.sim_settings = jsondecode(fileread(strcat(proj_folder,'\simulation_settings.json')));
 myCFD.transport_prop = jsondecode(fileread(strcat(proj_folder,'\transport_prop.json')));
 myCFD.boundaries.Ux = jsondecode(fileread(strcat(proj_folder,'\boundaries\Ux.json')));
 myCFD.boundaries.Uy = jsondecode(fileread(strcat(proj_folder,'\boundaries\Uy.json')));
@@ -30,10 +29,10 @@ myCFD.Solution.Uy = zeros(size(myCFD.Mesh.Nodes,2),1);
 myCFD.Solution.p = zeros(size(myCFD.Mesh.Nodes,2),1);
 
 %create waitbar
-%bar = waitbar(0,'1','Name','calculating solution...',...
-%    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
-%setappdata(bar,'canceling',0);
-for outerloop = 1:n_it
+bar = waitbar(0,'computing solution...','Name','calculating solution...',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+setappdata(bar,'canceling',0);
+for outerloop = 1:myCFD.sim_settings.num_iter
 %% Momentum predictor
 % assemble matrix for ux
 [M,f] = ux_BuildMatricesandVectors(myCFD);
@@ -41,12 +40,11 @@ for outerloop = 1:n_it
 
 % Solve system
 myCFD.Solution.Ux = solvematrix_momentum_x(myCFD,M,f);
-pdeplot(myCFD.Mesh.Nodes,myCFD.Mesh.Elements','XYData',myCFD.Solution.Ux)
+%pdeplot(myCFD.Mesh.Nodes,myCFD.Mesh.Elements','XYData',myCFD.Solution.Ux)
 %calculate residuals
 myCFD.Residual.Ax = diag(M);
 A = diag(diag(M));
 myCFD.Residual.Hx = (M-A)*myCFD.Solution.Ux;
-
 
 % assemble matrix for uy
 [M,f] = uy_BuildMatricesandVectors(myCFD);
@@ -73,13 +71,13 @@ myCFD.Solution.Ux = Ux;
 myCFD.Solution.Uy = Uy;
 
 %waitbar controls
-%waitbar(outerloop/n_it,bar,'computing solution');
+waitbar(outerloop/n_it,bar,'computing solution...');
 % Check for clicked Cancel button
-%if getappdata(bar,'canceling')
-%    break
-%end
+if getappdata(bar,'canceling')
+    break
+end
 end%end of outer loop
-%delete(bar)
+delete(bar)
 % Post Processing
 myCFD.Solution.U = sqrt(myCFD.Solution.Ux.^2+myCFD.Solution.Uy.^2);
 
