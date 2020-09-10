ebd9d586d6e7fc744445df4e408a11985298f67f
Emiel <Emiel@192.168.2.100>
2020-09-10 21:10:22 +0200

    simplify momentum element matricies and vectors

--- FEM_lib/ux_GenerateBoundaryElementMatrix.m
+++ FEM_lib/ux_GenerateBoundaryElementMatrix.m
@@ -1,4 +1,4 @@
-function [BMelem] = ux_GenerateBoundaryElementMatrix(el_index,myCFD)
+function [BMelem] = ux_GenerateBoundaryElementMatrix(~,myCFD)
 %GENERATEBOUNDARYELEMENTMATRIX Summary of this function goes here
 %   Detailed explanation goes here
 clear xc
@@ -6,34 +6,34 @@ clear yc
 clear BMelem
 
 topologybnd = myCFD.Mesh.topology.boundary;
-elmatbnd = myCFD.Mesh.BndElements;
-x = myCFD.Mesh.Nodes(1,:);
-y = myCFD.Mesh.Nodes(2,:);
-
-nu = myCFD.transport_prop.nu;
-bndtag = myCFD.Mesh.PhysicalTag;
-xc = zeros(1,topologybnd);
-yc = zeros(1,topologybnd);
-for index1=1:topologybnd
-	xc(index1) = x(elmatbnd(el_index,index1));
-	yc(index1) = y(elmatbnd(el_index,index1));
-end
-
-%lek = sqrt((xc(2)-xc(1))^2 + (yc(2)-yc(1))^2);
-[nx,ny] = FindNormal(elmatbnd(el_index,:),myCFD.Mesh);
+% elmatbnd = myCFD.Mesh.BndElements;
+% x = myCFD.Mesh.Nodes(1,:);
+% y = myCFD.Mesh.Nodes(2,:);
+% 
+% nu = myCFD.transport_prop.nu;
+% bndtag = myCFD.Mesh.PhysicalTag;
+% xc = zeros(1,topologybnd);
+% yc = zeros(1,topologybnd);
+% for index1=1:topologybnd
+% 	xc(index1) = x(elmatbnd(el_index,index1));
+% 	yc(index1) = y(elmatbnd(el_index,index1));
+% end
+% 
+% %lek = sqrt((xc(2)-xc(1))^2 + (yc(2)-yc(1))^2);
+% [nx,ny] = FindNormal(elmatbnd(el_index,:),myCFD.Mesh);
 BMelem = zeros(topologybnd);
-for index1=1:topologybnd
-    globalindex1 = elmatbnd(el_index,index1);
-    for index2 = 1:topologybnd
-        boundarytype = myCFD.boundaries.Ux.(bndtag(globalindex1)).type;
-        
-        %BMelem(index1,index2) = -nu*(beta(index2)*nx+gamma(index2)*ny)*abs(Delta)/2; %Transfer_rate_Coeff*lek/6*(1+double(eq(index1,index2)));
-        if strcmp(boundarytype,"Fixed value")
-            BMelem(index1,index2) = 0;
-        end
-        if strcmp(boundarytype,"Zero gradient")
-            BMelem(index1,index2) = 0;
-        end
-    end
-end
+% for index1=1:topologybnd
+%     globalindex1 = elmatbnd(el_index,index1);
+%     for index2 = 1:topologybnd
+%         boundarytype = myCFD.boundaries.Ux.(bndtag(globalindex1)).type;
+%         
+%         %BMelem(index1,index2) = -nu*(beta(index2)*nx+gamma(index2)*ny)*abs(Delta)/2; %Transfer_rate_Coeff*lek/6*(1+double(eq(index1,index2)));
+%         if strcmp(boundarytype,"Fixed value")
+%             BMelem(index1,index2) = 0;
+%         end
+%         if strcmp(boundarytype,"Zero gradient")
+%             BMelem(index1,index2) = 0;
+%         end
+%     end
+% end
 end
--- FEM_lib/ux_GenerateElementVector.m
+++ FEM_lib/ux_GenerateElementVector.m
@@ -27,15 +27,15 @@ B_mat = [1 xc(1) yc(1);1 xc(2) yc(2);1 xc(3) yc(3)] \  eye(3);
 
 %alpha = B_mat(1,1:3);
 beta  = B_mat(2,1:3);
-gamma = B_mat(3,1:3);
+% gamma = B_mat(3,1:3);
 
 sum_cpbeta = 0;
-sum_cpgamma = 0;
+% sum_cpgamma = 0;
 for index1 = 1:topology
 	global_index = elmat(el_index,index1);
     
     sum_cpbeta = sum_cpbeta + cp(global_index)*beta(index1);
-    sum_cpgamma = sum_cpgamma + cp(global_index)*gamma(index1);
+%     sum_cpgamma = sum_cpgamma + cp(global_index)*gamma(index1);
 end
 felem(1:topology) = -sum_cpbeta*abs(Delta)/6;
 
